f0e47e77c1a6a29c9d5464710d72d2ffe5844426
fplbuddy <fplhelper7@gmail.com>
2021-12-28 15:57:24 +0100

    Hello

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 14:23:51 2021
+
+@author: philipwinchester
+"""
+
+import Functions as funcs
+funcs.git_push(r'/Users/philipwinchester/Sites/fplbuddy.github.io/.git','Auto')
+
+    
+        
+
