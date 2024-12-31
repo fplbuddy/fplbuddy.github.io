1f6ca4f98f1557cfe32eabcc90e106f00fc50bca
fplbuddy <fplhelper7@gmail.com>
2024-12-31 12:01:28 +0000

    Auto

--- Analysis_Functions.py
+++ Analysis_Functions.py
@@ -54,13 +54,10 @@ def PickPlayers(Data, GWHorizon, PlayingSpots, TotalSpots, LockedPlayers, Potent
     Budget
     Current
     swap to me made
-    '''    
-    PositionsToPick = TotalSpots - sum(LockedPlayers.values())
+    '''
+    PositionsToPick = TotalSpots - len(LockedPlayers)
 
-    PA_list = [key for key, count in PotentialAdditions.items() for _ in range(count)]
-    LP_list = [key for key, count in LockedPlayers.items() for _ in range(count)]
-
-    PA_groups = list(combinations(PA_list, PositionsToPick))
+    PA_groups = list(combinations(PotentialAdditions, PositionsToPick))
     PA_groups = list(set(PA_groups))
 
     # Create a new DataFrame with only those columns
@@ -69,7 +66,7 @@ def PickPlayers(Data, GWHorizon, PlayingSpots, TotalSpots, LockedPlayers, Potent
     
     Data = Data.iloc[:, :GWHorizon+1]
 
-    columns =  [f'Fixed {i+1}' for i in range(len(LP_list))] +  [f'PA {i+1}' for i in range(PositionsToPick)] +  [ "Score" ]
+    columns =  [f'Fixed {i+1}' for i in range(len(LockedPlayers))] +  [f'PA {i+1}' for i in range(PositionsToPick)] +  [ "Score" ]
 
     # Create an empty DataFrame with these columns
     df = pd.DataFrame(columns=columns)
@@ -77,17 +74,19 @@ def PickPlayers(Data, GWHorizon, PlayingSpots, TotalSpots, LockedPlayers, Potent
     arr = np.empty((TotalSpots, GWHorizon))
 
     # Get the locked positions
-    for i in range( len(LP_list) ):
-        arr[i] = Data[Data["Player Name"] == LP_list[i]].values[0][1:]
-
+    for i in range( len(LockedPlayers) ):
+        arr[i] = Data[Data["Player Name"] == LockedPlayers[i]].values[0][1:]
+    print( len(PA_groups) )
     # Get the potential additions
     for i in range( len(PA_groups) ):
+        if i % 1000 == 0:
+            print(i)
         for j in range( len(PA_groups[i]) ):
             arr[-(j+1)] = Data[Data["Player Name"] == PA_groups[i][j]].values[0][1:]
         score = round(np.sum(np.sum(np.partition(arr, -PlayingSpots, axis=0)[-PlayingSpots:], axis=0)),2)
 
         
-        df = df.append(pd.DataFrame([np.append(LP_list, PA_groups[i]).tolist() + [str(score)]], columns=columns))
+        df = df.append(pd.DataFrame([np.append(LockedPlayers, PA_groups[i]).tolist() + [str(score)]], columns=columns))
 
     df.to_csv("PlayerSelect.csv", index=False)
 
