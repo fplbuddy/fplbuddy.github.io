24653eb2c518566a9660c6c62011f4694d7ec1b4
fplbuddy <fplhelper7@gmail.com>
2025-02-09 18:56:34 +0000

    Auto

--- tabledata.js
+++ tabledata.js
@@ -1,6 +1,6 @@
 const expectedGoalsData = {
   "Arsenal": { scores: [2.73, 2.8, 1.81, 1.93, 2.16, 2.12, 1.58, 2.5, 2.73, 1.95, 2.0, 1.35, 2.09, 3.13], logo: "logos/Arsenal2.png", page: "Arsenal.html" },
-"Aston Villa": { scores: [2.3, 1.62, 1.3, 1.67, 1.14, 1.58, 1.52, 2.35, 1.57, 1.19, 1.59, 1.33, 1.77, 1.45], logo: "logos/Aston_Villa2.png", page: "Aston_Villa.html" },
+"Aston Villa": { scores: [3.44, 1.62, 1.3, 1.67, 0.0, 1.58, 1.52, 2.35, 1.57, 1.19, 1.59, 1.33, 1.77, 1.45], logo: "logos/Aston_Villa2.png", page: "Aston_Villa.html" },
 "Bournemouth": { scores: [2.37, 2.08, 1.59, 1.59, 1.89, 2.32, 1.88, 1.61, 1.31, 1.64, 0.82, 1.76, 1.2, 2.32], logo: "logos/Bournemouth2.png", page: "Bournemouth.html" },
 "Brentford": { scores: [1.88, 2.06, 1.34, 1.75, 1.34, 1.4, 1.63, 0.81, 1.79, 1.36, 1.63, 2.06, 1.6, 1.85], logo: "logos/Brentford2.png", page: "Brentford.html" },
 "Brighton": { scores: [1.4, 2.04, 1.3, 1.38, 1.03, 1.51, 1.13, 2.0, 1.45, 1.82, 1.36, 1.59, 0.99, 1.37], logo: "logos/Brighton2.png", page: "Brighton.html" },
@@ -10,7 +10,7 @@ const expectedGoalsData = {
 "Fulham": { scores: [1.36, 1.31, 1.64, 1.41, 1.58, 0.72, 1.02, 1.19, 1.45, 2.1, 1.39, 1.19, 1.5, 1.2], logo: "logos/Fulham2.png", page: "Fulham.html" },
 "Ipswich": { scores: [1.07, 1.23, 1.0, 0.9, 1.06, 0.92, 1.43, 1.0, 0.63, 0.97, 0.82, 1.3, 1.42, 1.45], logo: "logos/Ipswich2.png", page: "Ipswich.html" },
 "Leicester": { scores: [0.6, 1.24, 1.23, 0.95, 1.07, 0.79, 1.03, 1.04, 0.75, 1.21, 1.74, 0.9, 1.52, 0.88], logo: "logos/Leicester2.png", page: "Leicester.html" },
-"Liverpool": { scores: [2.94, 1.7, 2.23, 3.74, 2.21, 1.9, 2.02, 2.99, 2.92, 2.52, 2.05, 1.3, 2.25, 2.08], logo: "logos/Liverpool2.png", page: "Liverpool.html" },
+"Liverpool": { scores: [5.15, 1.7, 2.23, 3.74, 0.0, 1.9, 2.02, 2.99, 2.92, 2.52, 2.05, 1.3, 2.25, 2.08], logo: "logos/Liverpool2.png", page: "Liverpool.html" },
 "Man City": { scores: [2.15, 1.56, 2.16, 1.86, 2.43, 3.15, 1.98, 2.0, 1.62, 2.38, 2.83, 3.21, 2.05, 1.94], logo: "logos/Man_City2.png", page: "Man_City.html" },
 "Man United": { scores: [1.29, 0.97, 1.89, 0.75, 1.68, 1.12, 1.1, 1.15, 1.7, 1.09, 1.37, 1.72, 1.18, 1.43], logo: "logos/Man_United2.png", page: "Man_United.html" },
 "Newcastle": { scores: [1.43, 1.83, 1.22, 2.24, 1.75, 2.25, 2.46, 1.95, 1.86, 2.76, 1.9, 1.94, 0.97, 1.6], logo: "logos/Newcastle2.png", page: "Newcastle.html" },
@@ -24,7 +24,7 @@ const expectedGoalsData = {
 
 const defensiveScoreData = {
   "Arsenal": { scores: [2.74, 2.51, 2.13, 2.37, 1.84, 2.42, 2.84, 2.22, 2.66, 2.54, 2.21, 1.37, 1.89, 3.11], logo: "logos/Arsenal2.png", page: "Arsenal.html" },
-"Aston Villa": { scores: [1.71, 0.74, 1.17, 0.87, 0.55, 1.1, 1.17, 2.01, 0.78, 0.46, 1.25, 0.86, 0.78, 1.2], logo: "logos/Aston_Villa2.png", page: "Aston_Villa.html" },
+"Aston Villa": { scores: [2.26, 0.74, 1.17, 0.87, 0.0, 1.1, 1.17, 2.01, 0.78, 0.46, 1.25, 0.86, 0.78, 1.2], logo: "logos/Aston_Villa2.png", page: "Aston_Villa.html" },
 "Bournemouth": { scores: [2.29, 1.61, 1.37, 0.84, 1.31, 1.98, 1.39, 1.52, 1.43, 1.67, 0.68, 1.32, 0.64, 2.08], logo: "logos/Bournemouth2.png", page: "Bournemouth.html" },
 "Brentford": { scores: [1.01, 1.45, 1.77, 0.94, 0.75, 0.52, 0.63, 0.41, 1.17, 0.86, 1.27, 1.36, 1.12, 1.01], logo: "logos/Brentford2.png", page: "Brentford.html" },
 "Brighton": { scores: [0.71, 1.98, 1.02, 1.22, 0.44, 1.03, 1.13, 1.76, 0.84, 1.3, 0.75, 1.1, 0.53, 0.6], logo: "logos/Brighton2.png", page: "Brighton.html" },
@@ -34,7 +34,7 @@ const defensiveScoreData = {
 "Fulham": { scores: [1.32, 1.53, 1.29, 1.25, 0.92, 0.6, 0.66, 1.0, 0.87, 2.18, 1.02, 2.07, 1.01, 0.72], logo: "logos/Fulham2.png", page: "Fulham.html" },
 "Ipswich": { scores: [0.5, 0.43, 0.75, 0.73, 0.73, 0.49, 0.87, 0.29, 0.32, 0.31, 1.2, 0.64, 1.1, 0.87], logo: "logos/Ipswich2.png", page: "Ipswich.html" },
 "Leicester": { scores: [0.33, 0.64, 0.7, 0.29, 0.93, 0.21, 0.43, 0.68, 0.27, 0.71, 1.72, 0.58, 1.21, 0.49], logo: "logos/Leicester2.png", page: "Leicester.html" },
-"Liverpool": { scores: [2.11, 1.05, 1.48, 2.94, 1.6, 2.66, 1.8, 2.1, 2.36, 1.49, 1.22, 1.29, 1.86, 2.14], logo: "logos/Liverpool2.png", page: "Liverpool.html" },
+"Liverpool": { scores: [3.71, 1.05, 1.48, 2.94, 0.0, 2.66, 1.8, 2.1, 2.36, 1.49, 1.22, 1.29, 1.86, 2.14], logo: "logos/Liverpool2.png", page: "Liverpool.html" },
 "Man City": { scores: [1.2, 0.92, 1.01, 1.42, 1.78, 2.28, 1.67, 1.85, 2.18, 1.52, 1.82, 2.49, 1.5, 1.51], logo: "logos/Man_City2.png", page: "Man_City.html" },
 "Man United": { scores: [0.72, 1.82, 1.83, 0.73, 1.71, 1.09, 0.69, 0.71, 1.46, 0.97, 0.98, 1.46, 0.67, 1.18], logo: "logos/Man_United2.png", page: "Man_United.html" },
 "Newcastle": { scores: [0.58, 1.35, 0.54, 1.31, 1.56, 1.23, 1.78, 1.59, 1.04, 1.9, 1.28, 0.89, 0.62, 2.1], logo: "logos/Newcastle2.png", page: "Newcastle.html" },
