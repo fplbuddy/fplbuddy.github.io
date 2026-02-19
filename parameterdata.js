aeb849709b48db2cc1e940f48217a7a89e663ab5
fplbuddy <fplhelper7@gmail.com>
2026-02-19 23:06:27 +0000

    Auto

--- parameterdata.js
+++ parameterdata.js
@@ -1,7 +1,7 @@
 const data = {
-"Arsenal": { scores: [1.3, 0.73], logo: "logos/Arsenal2.png", page: "Arsenal.html" },
+"Arsenal": { scores: [1.3, 0.77], logo: "logos/Arsenal2.png", page: "Arsenal.html" },
 "Aston Villa": { scores: [1.05, 1.02], logo: "logos/Aston_Villa2.png", page: "Aston_Villa.html" },
-"Bournemouth": { scores: [1.15, 1.41], logo: "logos/Bournemouth2.png", page: "Bournemouth.html" },
+"Bournemouth": { scores: [1.15, 1.4], logo: "logos/Bournemouth2.png", page: "Bournemouth.html" },
 "Brentford": { scores: [1.17, 1.26], logo: "logos/Brentford2.png", page: "Brentford.html" },
 "Brighton": { scores: [1.05, 1.3], logo: "logos/Brighton2.png", page: "Brighton.html" },
 "Burnley": { scores: [0.65, 1.89], logo: "logos/Burnley2.png", page: "Burnley.html" },
@@ -10,15 +10,15 @@ const data = {
 "Everton": { scores: [0.78, 1.03], logo: "logos/Everton2.png", page: "Everton.html" },
 "Fulham": { scores: [0.98, 1.37], logo: "logos/Fulham2.png", page: "Fulham.html" },
 "Leeds": { scores: [0.87, 1.68], logo: "logos/Leeds2.png", page: "Leeds.html" },
-"Liverpool": { scores: [1.29, 1.16], logo: "logos/Liverpool2.png", page: "Liverpool.html" },
+"Liverpool": { scores: [1.28, 1.16], logo: "logos/Liverpool2.png", page: "Liverpool.html" },
 "Man City": { scores: [1.37, 0.94], logo: "logos/Man_City2.png", page: "Man_City.html" },
-"Man United": { scores: [1.09, 1.31], logo: "logos/Man_United2.png", page: "Man_United.html" },
+"Man United": { scores: [1.08, 1.3], logo: "logos/Man_United2.png", page: "Man_United.html" },
 "Newcastle": { scores: [1.12, 1.29], logo: "logos/Newcastle2.png", page: "Newcastle.html" },
 "Nott'm Forest": { scores: [0.84, 1.24], logo: "logos/Nott'm_Forest2.png", page: "Nott'm_Forest.html" },
 "Sunderland": { scores: [0.7, 1.42], logo: "logos/Sunderland2.png", page: "Sunderland.html" },
-"Tottenham": { scores: [1.01, 1.46], logo: "logos/Tottenham2.png", page: "Tottenham.html" },
+"Tottenham": { scores: [1.0, 1.46], logo: "logos/Tottenham2.png", page: "Tottenham.html" },
 "West Ham": { scores: [0.87, 1.6], logo: "logos/West_Ham2.png", page: "West_Ham.html" },
-"Wolves": { scores: [0.67, 1.57], logo: "logos/Wolves2.png", page: "Wolves.html" },
+"Wolves": { scores: [0.7, 1.57], logo: "logos/Wolves2.png", page: "Wolves.html" },
 
 };
 
